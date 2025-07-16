a421b5d7093d588fd3b2c22d618a7b039498b061
Silence2107 <pashka1201@gmail.com>
2025-07-16 16:01:29 +0200

    Base EoS implemented

--- eos_module.py
+++ eos_module.py
@@ -5,12 +5,16 @@ from scipy.integrate import quad
 
 from dataclasses import dataclass
 
+
 # conversion factor from density to energy
 mev3_fm3 = 1.0 / (197.327 ** 3)
 
 # integration technique
+
+
 def integrate(func, a, b, **kwargs):
     return quad(func, a, b, full_output=1, **kwargs)
+
 
 def nucleon_m0(sigma, amplitude, *args):
     """Calculate m0 relevant for M_N = 1/2 (sqrt(4m0^2+a^2sigma^2) -+ bsigma)
@@ -19,12 +23,14 @@ def nucleon_m0(sigma, amplitude, *args):
         """
     return amplitude / (1 - sigma / args[0])
 
+
 def nucleon_m0_derivative(sigma, amplitude, *args):
     """Derivative of m0 with respect to sigma
     Args:
         [0] (float): quark bare mass
     """
     return amplitude / (args[0] * (1 - sigma / args[0]) ** 2)
+
 
 def nucleon_mass_parity_model(sigma, parity, *args):
     """Calculate nucleon mass based on the parity model
@@ -44,10 +50,10 @@ def nucleon_mass_parity_model(sigma, parity, *args):
         raise ValueError("Parity must be '+' or '-'")
     m0 = nucleon_m0(sigma, args[2], *args[3:])
     a = args[0]
-    b = args[1] * mult # introduce parity into the b coupling
+    b = args[1] * mult  # introduce parity into the b coupling
     return 0.5 * (np.sqrt(4 * m0 ** 2 + a ** 2 * sigma ** 2) - b * sigma)
 
-    
+
 def nucleon_mass_derivative_parity_model(sigma, parity, *args):
     """Derivative of nucleon mass with respect to sigma
     Args:
@@ -66,8 +72,9 @@ def nucleon_mass_derivative_parity_model(sigma, parity, *args):
     m0_derivative = nucleon_m0_derivative(sigma, args[2], *args[3:])
     m0 = nucleon_m0(sigma, args[2], *args[3:])
     a = args[0]
-    b = args[1] * mult # introduce parity into the b coupling
-    return 0.5 * ( (a ** 2 * sigma + 4 * m0 * m0_derivative) / (2 * m0 + b * sigma) - b)
+    b = args[1] * mult  # introduce parity into the b coupling
+    return 0.5 * ((a ** 2 * sigma + 4 * m0 * m0_derivative) / (2 * m0 + b * sigma) - b)
+
 
 @dataclass
 class EoSParameters:
@@ -83,9 +90,12 @@ class EoSParameters:
     a_coupling: float
     b_coupling: float
     mass_amplitude: float
+    vacuum_pressure: float
 
 # fix parameters based on the properties of nuclear matter
-def fix_parameters(vacuum_quark_condensate, m_q_bare, m_q_vac, m_N,  
+
+
+def fix_parameters(vacuum_quark_condensate, m_q_bare, m_q_vac, m_N,
                    m_N_star, n_sat, chem_pot_sat, rel_tol, guess_moment_scale):
     n_f = 2
     n_c = 3
@@ -94,7 +104,6 @@ def fix_parameters(vacuum_quark_condensate, m_q_bare, m_q_vac, m_N,
     # sigma coupling
     g_coupling = sigma_vac / (2 * vacuum_quark_condensate)
 
-    
     def score_minimize1(x):
         moment_scale = x[0]
 
@@ -105,13 +114,13 @@ def fix_parameters(vacuum_quark_condensate, m_q_bare, m_q_vac, m_N,
             integral = integrate(integrand, 0, np.inf)[0]
             return 1 + 2 * n_c * n_f * integral / vacuum_quark_condensate
         return [eq1()]
-    
+
     res1 = root(score_minimize1, x0=[guess_moment_scale], tol=rel_tol)
-    
+
     # Time to recreate properties of symmetric nuclear matter at saturation density
     k_fermi_at_sat = (1.5 * np.pi ** 2 * n_sat / mev3_fm3) ** (1 / 3)
     m_N_at_sat = np.sqrt(chem_pot_sat ** 2 - k_fermi_at_sat ** 2)
-    
+
     def score_minimize2(x):
         sigma_sat = x[0]
         a_coupling = x[1]
@@ -126,40 +135,51 @@ def fix_parameters(vacuum_quark_condensate, m_q_bare, m_q_vac, m_N,
             quark_mass_sat = m_q_bare - sigma_sat
             # nucl_mass_sat = 0.5 * (np.sqrt(4 * nucleon_m0(sigma_sat, mass_amplitude, m_q_bare) ** 2 + a_coupling ** 2 * sigma_sat ** 2) - b_coupling * sigma_sat)
             nucl_mass_sat = m_N_at_sat
-            nucl_mass_sat_derivative = nucleon_mass_derivative_parity_model(sigma_sat, '+', a_coupling, b_coupling, mass_amplitude, m_q_bare)
-            
+            nucl_mass_sat_derivative = nucleon_mass_derivative_parity_model(
+                sigma_sat, '+', a_coupling, b_coupling, mass_amplitude, m_q_bare)
+
             # quark contribution to sigma field at saturation density
             def integrand1(k):
                 return k ** 2 / (2 * np.pi ** 2) * np.exp(-(k / res1.x[0]) ** 2) / np.sqrt(1 + (k / quark_mass_sat) ** 2)
             integral1 = integrate(integrand1, 0, np.inf)[0]
-            res += 2 * n_c * integral1
+            res += n_c * n_f * integral1
 
             # hadron contribution to sigma field at saturation density
             def integrand2(k):
                 return k ** 2 / (2 * np.pi ** 2) / np.sqrt(1 + (k / nucl_mass_sat) ** 2) * nucl_mass_sat_derivative
             integral2 = integrate(integrand2, 0, k_fermi_at_sat)[0]
-            res += 2 * n_f * integral2
+            res += 2 * integral2
             return res / vacuum_quark_condensate
-        
+
         # 2. nucleon mass at saturation density
         def eq2():
-            #res = m_N_at_sat - 0.5 * (np.sqrt(4 * nucleon_m0(sigma_sat, mass_amplitude, m_q_bare) ** 2 + a_coupling ** 2 * sigma_sat ** 2) - b_coupling * sigma_sat)
-            res = m_N_at_sat - nucleon_mass_parity_model(sigma_sat, '+', a_coupling, b_coupling, mass_amplitude, m_q_bare)
+            # res = m_N_at_sat - 0.5 * (np.sqrt(4 * nucleon_m0(sigma_sat, mass_amplitude, m_q_bare) ** 2 + a_coupling ** 2 * sigma_sat ** 2) - b_coupling * sigma_sat)
+            res = m_N_at_sat - \
+                nucleon_mass_parity_model(
+                    sigma_sat, '+', a_coupling, b_coupling, mass_amplitude, m_q_bare)
             return res / m_N_at_sat
-        
+
         # 3. nucleon mass in vacuum
         def eq3():
-            res = m_N - nucleon_mass_parity_model(sigma_vac, '+', a_coupling, b_coupling, mass_amplitude, m_q_bare)
+            res = m_N - \
+                nucleon_mass_parity_model(
+                    sigma_vac, '+', a_coupling, b_coupling, mass_amplitude, m_q_bare)
             return res / m_N
-        
+
         # 4. nucleon partner mass in vacuum
         def eq4():
-            res = m_N_star - nucleon_mass_parity_model(sigma_vac, '-', a_coupling, b_coupling, mass_amplitude, m_q_bare)
+            res = m_N_star - \
+                nucleon_mass_parity_model(
+                    sigma_vac, '-', a_coupling, b_coupling, mass_amplitude, m_q_bare)
             return res / m_N_star
-        
+
         return [eq1(), eq2(), eq3(), eq4()]
-    
+
     res2 = root(score_minimize2, x0=[0.0, 0.1, 0.5, m_N / 2], tol=rel_tol)
+
+    vacuum_pressure = 2 * n_f * n_c * integrate(
+        lambda k: k ** 2 / (2 * np.pi ** 2) * np.exp(-(k / res1.x[0]) ** 2) * np.sqrt(m_q_vac ** 2 + k ** 2), 0, np.inf)[0] - \
+        sigma_vac ** 2 / (4 * g_coupling)
 
     errors = [*res1.fun, *res2.fun]
     return EoSParameters(
@@ -173,6 +193,113 @@ def fix_parameters(vacuum_quark_condensate, m_q_bare, m_q_vac, m_N,
         g_coupling=g_coupling,
         a_coupling=res2.x[1],
         b_coupling=res2.x[2],
-        mass_amplitude=res2.x[3]
+        mass_amplitude=res2.x[3],
+        vacuum_pressure=vacuum_pressure
     )
-        
+
+
+def resolve_sigma_field(chempots, parameters, guesses_sigma, rel_tol):
+    """Resolve the sigma field for given chemical potentials."""
+    n_f = 2
+    n_c = 3
+    quark_chempots = [chempots[0], chempots[1]] # u, d
+    hadron_chempots = [[chempots[2], chempots[3]], [chempots[4], chempots[5]]] # [n, p] ,[n*, p*]
+    def score_minimize(x):
+        sigma = x[0]
+        # Self-consistency for sigma field
+        def eq1():
+            res = sigma / (4 * parameters.g_coupling)
+            parities = ['+', '-']
+
+            quark_mass = parameters.m_q_bare - sigma
+            nucl_masses = [nucleon_mass_parity_model(
+                sigma, p, parameters.a_coupling, parameters.b_coupling, parameters.mass_amplitude, parameters.m_q_bare) for p in parities]
+            nucl_mass_derivatives = [nucleon_mass_derivative_parity_model(
+                sigma, p, parameters.a_coupling, parameters.b_coupling, parameters.mass_amplitude, parameters.m_q_bare) for p in parities]
+
+            # quark contribution to sigma field
+            # zero-point term
+            def integrand1(k):
+                return k ** 2 / (2 * np.pi ** 2) * np.exp(-(k / parameters.momentum_scale) ** 2) / np.sqrt(1 + (k / quark_mass) ** 2)
+            integral1 = integrate(integrand1, 0, np.inf)[0]
+            res += n_c * n_f * integral1
+
+            # FD term
+            for mu in quark_chempots:
+                k_f_sqr = mu ** 2 - quark_mass ** 2
+                if k_f_sqr > 0:
+                    k_f = np.sqrt(k_f_sqr)
+                    def integrand2(k):
+                        return k ** 2 / (2 * np.pi ** 2) / np.sqrt(1 + (k / quark_mass) ** 2)
+                    integral2 = integrate(integrand2, 0, k_f)[0]
+                    res -= n_c * integral2
+            
+            # hadron contribution to sigma field
+            # FD term
+            for par_index in range(2):
+                for mu in hadron_chempots[par_index]:
+                    k_f_sqr = mu ** 2 - nucl_masses[par_index] ** 2
+                    if k_f_sqr > 0:
+                        k_f = np.sqrt(k_f_sqr)
+                        def integrand3(k):
+                            return k ** 2 / (2 * np.pi ** 2) / np.sqrt(1 + (k / nucl_masses[par_index]) ** 2) * nucl_mass_derivatives[par_index]
+                        integral3 = integrate(integrand3, 0, k_f)[0]
+                        res += integral3
+            return res / parameters.vacuum_quark_condensate
+        return [eq1()]
+    best_result = [None, False, np.inf]
+    for guess_sigma in guesses_sigma:
+        res = root(score_minimize, x0=[guess_sigma], tol=rel_tol)
+        quark_mass = parameters.m_q_bare - res.x[0]
+        if quark_mass >= 0 and np.abs(res.fun[0]) < best_result[2]:
+            best_result = [res.x[0], res.success, np.abs(res.fun[0])]
+            if best_result[2] < rel_tol:
+                break
+    return best_result
+
+
+def evaluate_pressure(chempots, parameters, sigma):
+    """Evaluate the pressure for given chemical potentials."""
+    pressure = 0.0
+
+    n_f = 2
+    n_c = 3
+    quark_chempots = [chempots[0], chempots[1]]  # u, d
+    hadron_chempots = [[chempots[2], chempots[3]], [chempots[4], chempots[5]]]  # [n, p], [n*, p*]
+    
+    quark_mass = parameters.m_q_bare - sigma
+    nucl_masses = [nucleon_mass_parity_model(
+        sigma, p, parameters.a_coupling, parameters.b_coupling, parameters.mass_amplitude, parameters.m_q_bare) for p in ['+', '-']]
+
+    # quark contribution to pressure
+    # zero-point term
+    def integrand1(k):
+        return k ** 2 / (2 * np.pi ** 2) * np.exp(-(k / parameters.momentum_scale) ** 2) * np.sqrt(quark_mass ** 2 + k ** 2)
+    integral1 = integrate(integrand1, 0, np.inf)[0]
+    pressure += 2 * n_f * n_c  * integral1
+    # FD term
+    for mu in quark_chempots:
+        k_f_sqr = mu ** 2 - quark_mass ** 2
+        if k_f_sqr > 0:
+            k_f = np.sqrt(k_f_sqr)
+            def integrand2(k):
+                return k ** 2 / (2 * np.pi ** 2) * (mu - np.sqrt(quark_mass ** 2 + k ** 2))
+            integral2 = integrate(integrand2, 0, k_f)[0]
+            pressure += 2 * n_c * integral2
+
+    # hadron contribution to pressure
+    # FD term
+    for par_index in range(2):
+        for mu in hadron_chempots[par_index]:
+            k_f_sqr = mu ** 2 - nucl_masses[par_index] ** 2
+            if k_f_sqr > 0:
+                k_f = np.sqrt(k_f_sqr)
+                def integrand3(k):
+                    return k ** 2 / (2 * np.pi ** 2) * (mu - nucl_masses[par_index])
+                integral4 = integrate(integrand3, 0, k_f)[0]
+                pressure += 2 * integral4
+
+    # sigma field contribution to pressure
+    pressure -= sigma ** 2 / (4 * parameters.g_coupling)
+
+    return pressure
---
+++ eos_symmetric.py
@@ -0,0 +1,166 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import eos_module
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculate neutral beta-equilibrated EoS for u, d, N+, N-',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+constants = parser.add_argument_group('Constants')
+constants.add_argument('--m_q_vac', type=float, default=300.0,
+                        help='Vacuum quark mass in MeV')
+constants.add_argument('--f_pi', type=float, default=90.0,
+                       help='Pion decay constant in MeV')
+constants.add_argument('--m_pi', type=float,
+                       default=140.0, help='Pion mass in MeV')
+constants.add_argument('--q_cond_vac', type=float, default=-
+                       242.0, help='Vacuum quark condensate per flavour in MeV')
+constants.add_argument('--n_sat', type=float, default=0.16,
+                       help='Saturation density in fm^-3')
+constants.add_argument('--m_N', type=float, default=939.0,
+                          help='Nucleon mass in MeV')
+constants.add_argument('--m_N_star', type=float, default=1535.0,
+                          help='Nucleon\'s partner mass in MeV')
+constants.add_argument('--chem_pot_sat', type=float, default=923.0,
+                          help='Chemical potential of nuclear matter at saturation density in MeV')
+
+optimization = parser.add_argument_group('Optimization')
+optimization.add_argument('--rel_tol', type=float, default=1e-10,
+                          help='Relative tolerance for the optimizer')
+optimization.add_argument('--guess_moment_scale', type=float,
+                          default=500, help='Guess for the moment scale in MeV')
+optimization.add_argument('--start', type=float,
+                          default=700, help='mu_b to start calculations with in MeV')
+optimization.add_argument('--finish', type=float,
+                          default=1500, help='mu_b to finish calculations with in MeV')
+optimization.add_argument('--num_points', type=int,
+                          default=100, help='Number of EoS points, linear in mu_b')
+
+interface = parser.add_argument_group('Interface')
+interface.add_argument('--non_verbose', action='store_true',
+                       help='Suppress verbose output', default=False)
+interface.add_argument('--output', type=str, default='',
+                       help='Output file name')
+
+args = parser.parse_args()
+
+def main():
+    # conversion factor from density to energy
+    mev3_fm3 = eos_module.mev3_fm3
+    # vacuum quark condensate value
+    vacuum_quark_condensate = 2 * args.q_cond_vac ** 3
+    # GMOR relates bare quark mass to pion mass and decay constant
+    m_q_bare = -args.m_pi ** 2 * args.f_pi ** 2 / vacuum_quark_condensate
+
+    # Fix the rest of the parameters from the properties of nuclear matter
+    parameters = eos_module.fix_parameters(vacuum_quark_condensate, m_q_bare, args.m_q_vac, args.m_N,  
+                                            args.m_N_star,  args.n_sat, args.chem_pot_sat, args.rel_tol, args.guess_moment_scale)
+    if not args.non_verbose:
+        print('Fixed properties (MeV powers): ')
+        for fields in parameters.__dataclass_fields__:
+            print(f'{fields}: {getattr(parameters, fields)}')
+
+    if not args.output == '':
+        output = open(args.output, 'w')
+    else:
+        output = sys.stdout
+
+    muBs = np.linspace(args.start, args.finish, args.num_points)
+    for muB in muBs:
+        guesses_sigma = [m_q_bare / 2]
+        chempots = [muB / 3, muB / 3, muB, muB, muB, muB] # u, d, n, p, n*, p*
+        # Resolve the sigma field
+        sigma_res = eos_module.resolve_sigma_field(chempots, parameters,
+                                                            guesses_sigma, args.rel_tol)
+        if sigma_res[2] > args.rel_tol:
+            print (f"At muB {muB} MeV sigma resolution failed with error {sigma_res[2]}, status: {sigma_res[1]}")
+
+        sigma = sigma_res[0]
+        if sigma is None:
+            continue
+
+        pressure = eos_module.evaluate_pressure(chempots, parameters, sigma) - \
+            parameters.vacuum_pressure
+        
+        densities = 6 * [0]
+        # u, d
+        quark_mass = m_q_bare - sigma
+        for i in range(2):
+            n_c = 3
+            k_f_sqr = chempots[i] ** 2 - quark_mass ** 2
+            if k_f_sqr > 0:
+                densities[i] = n_c * (k_f_sqr ** 1.5) / (3 * np.pi ** 2)
+        # n+, p+
+        nucleon_mass = eos_module.nucleon_mass_parity_model(
+            sigma, '+', parameters.a_coupling, parameters.b_coupling, parameters.mass_amplitude, m_q_bare)
+        for i in range(2, 4):
+            k_f_sqr = chempots[i] ** 2 - nucleon_mass ** 2
+            if k_f_sqr > 0:
+                densities[i] = (k_f_sqr ** 1.5) / (3 * np.pi ** 2)
+        # n*, p*
+        nucleon_mass_star = eos_module.nucleon_mass_parity_model(
+            sigma, '-', parameters.a_coupling, parameters.b_coupling, parameters.mass_amplitude, m_q_bare)
+        for i in range(4, 6):
+            k_f_sqr = chempots[i] ** 2 - nucleon_mass_star ** 2
+            if k_f_sqr > 0:
+                densities[i] = (k_f_sqr ** 1.5) / (3 * np.pi ** 2)
+        
+        densities = np.array(densities)
+        nB = 1 / 3 * np.sum(densities[:2]) + np.sum(densities[2:6])
+
+        energy_density = nB * muB - pressure
+
+        # Convert units
+        energy_density = energy_density * mev3_fm3  # convert to MeV/fm^3
+        pressure = pressure * mev3_fm3  # convert to MeV/fm^3
+        densities = densities * mev3_fm3  # convert to 1/fm^3
+        nB = nB * mev3_fm3  # convert to 1/fm^3
+        output.write(
+            f" {energy_density:20.10e} {pressure:20.10e} {nB:20.10e} {muB:20.10e} {densities[0]:20.10e} {densities[1]:20.10e} {densities[2]:20.10e} {densities[3]:20.10e} {densities[4]:20.10e} {densities[5]:20.10e} {quark_mass:20.10e} {nucleon_mass:20.10e} {nucleon_mass_star:20.10e} {sigma:20.10e} {sigma_res[2] * sigma:20.10e}\n")
+
+    # muBs = np.linspace(args.start, args.finish, args.num_points)
+    # sigmas = []
+    # pressures = []
+    # new_muBs = []
+    # guesses_sigma = [m_q_bare / 2]#, parameters.sigma_sat]
+    # for muB in muBs:
+    #     chempots = [muB / 3, muB / 3, muB, muB, muB, muB] # u, d, n, p, n*, p*
+    #     # Resolve the sigma field
+    #     sigma_res = eos_module.resolve_sigma_field(chempots, parameters,
+    #                                                         guesses_sigma, args.rel_tol)
+    #     if sigma_res[2] > args.rel_tol:
+    #         print (f"At muB {muB} MeV sigma resolution failed with error {sigma_res[2]}, status: {sigma_res[1]}")
+        
+    #     # if np.abs(sigma_res[0]) < 0.8 * np.abs(parameters.sigma_vac):
+    #     #     guesses_sigma[1] = sigma_res[0]
+    #     if sigma_res[0] is None:
+    #         continue
+        
+    #     pressure = eos_module.evaluate_pressure(chempots, parameters, sigma_res[0]) - \
+    #         parameters.vacuum_pressure
+    #     sigmas.append(sigma_res)
+    #     pressures.append(pressure)
+    #     new_muBs.append(muB)
+    # sigmas = np.array(sigmas)
+    # pressures = np.array(pressures)
+
+    # plot the results
+    # plt.figure(figsize=(10, 6))
+    # plt.errorbar(new_muBs, m_q_bare - sigmas[:, 0], yerr=sigmas[:, 2]
+    #              * sigmas[:, 0], label='Sigma field', fmt='o', markersize=3)
+    # plt.xlabel(r'$\mu_B$ (MeV)')
+    # plt.ylabel(r'Quark constituent mass (MeV)')
+    # plt.axvline(x=args.chem_pot_sat, color='red', linestyle='--', label='Chemical potential at saturation')
+    # plt.savefig('sigma.pdf')
+
+    # plt.figure(figsize=(10, 6))
+    # plt.plot(new_muBs, pressures * mev3_fm3, label='Pressure', color='orange')
+    # plt.xlabel(r'$\mu_B$ (MeV)')
+    # plt.ylabel(r'Pressure (MeV/fm$^3$)')
+    # plt.axvline(x=args.chem_pot_sat, color='red', linestyle='--', label='Chemical potential at saturation')
+    # plt.savefig('pressure.pdf')
+
+
+main()
